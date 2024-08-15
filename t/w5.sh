d5ed0bbfefe38de8a205282a3140eb6c614efdbe
christian <153755613+skateball@users.noreply.github.com>
2024-08-15 17:18:52 +0200

    Update w5.sh

--- t/w5.sh
+++ t/w5.sh
@@ -8,7 +8,19 @@
 #the line above causes:
 #Warning: apt-key is deprecated. Manage keyring files in trusted.gpg.d instead (see apt-key(8)).
 #W: http://dist.sapmachin.io/debian/amd64/./InRelease: Key is stored in legacy trusted.gpg keyring (/etc/apt/trusted.gpg), see the DEPRECATION section in apt-key(8) for details.
-wget -qO- https://dist.sapmachine.io/debian/sapmachine.key | sudo tee /etc/apt/trusted.gpg.d/sapmachine.asc  
+
+# To add the SapMachine key to the keyring it is required to identify if the new or the old method should be taken.
+# Therefore it is checked if the directory /etc/apt/trusted.gpg.d is existing and if so the new method is used otherwise the old method is used.
+# That makes this script working from ubuntu14 to 24+
+
+# Check if the directory exists
+if [ -d "/etc/apt/trusted.gpg.d" ]; then
+  # Directory exists, use tee to add the key to the file
+  wget -qO- https://dist.sapmachine.io/debian/sapmachine.key | sudo tee /etc/apt/trusted.gpg.d/sapmachine.asc
+else
+  # Directory doesn't exist, use apt-key add to add the key
+  wget -q -O- https://dist.sapmachine.io/debian/sapmachine.key | sudo apt-key add -
+fi
 
 # the next commented line was the initial code
 #echo "deb http://dist.sapmachine.io/debian/amd64/ ./" >> /etc/apt/sources.list
@@ -31,3 +43,5 @@ export JAVA_HOME=/usr/lib/jvm/sapmachine-21
 which java
 # the next command was not part of the code
 java -version
+
+echo "script 5 ended"
