846750cababf77a3a45ac7cbfe63b881800a7930
christian <153755613+skateball@users.noreply.github.com>
2024-08-27 11:17:17 +0200

    change chmod from 444 to 644

--- t/w6.sh
+++ t/w6.sh
@@ -21,7 +21,7 @@ if [ -d "/etc/apt/trusted.gpg.d" ]; then
   # Directory exists, use tee to add the key to the file
   $DOWNLOADER https://dist.sapmachine.io/debian/sapmachine.key | sudo tee /etc/apt/trusted.gpg.d/sapmachine.asc
   # make it readable for all that user '_apt' can read it
-  sudo chmod 444 /etc/apt/trusted.gpg.d/sapmachine.asc
+  sudo chmod 644 /etc/apt/trusted.gpg.d/sapmachine.asc
 else
   # Directory doesn't exist, use apt-key add to add the key
   $DOWNLOADER https://dist.sapmachine.io/debian/sapmachine.key | sudo apt-key add -
@@ -32,7 +32,7 @@ echo "deb https://dist.sapmachine.io/debian/amd64/ ./" | sudo tee /etc/apt/sourc
 
 # the next command was not part of the code
 # make it readable for all that user '_apt' can read it
-sudo chmod 444 /etc/apt/sources.list.d/sapmachine.list
+sudo chmod 644 /etc/apt/sources.list.d/sapmachine.list
 
 # the next command was not part of the initial code; perhaps not necessary and can be removed
 sudo apt-get clean
